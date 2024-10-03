c06f7d98879e84b76bc84af00e00f284038c2c38
sajad <sajadbanazadeh@gmail.com>
2024-10-03 14:00:35 +0330

    Implemented  function to return the sum of two numbers.

--- doctest_examples/math_operations.py
+++ doctest_examples/math_operations.py
@@ -2,5 +2,20 @@
 import doctest
 
 
+def add(x, y):
+    '''
+    Adds two numbers together.
+
+    >>> add(4, 5)
+    9
+    >>> add(-1, 1)
+    0
+    >>> add(2.5, 2.5)
+    5.0
+    '''
+    return x + y
+
+
+
 if __name__ == "__main__":
     doctest.testmod()
